8bd837b98af6e6887f67e47de37e11039ad29c91
tomoxpy16 <tomasospina16@gmail.com>
2025-02-24 21:28:53 -0500

    Update Modulo.c

--- Modulo.c
+++ Modulo.c
@@ -1,10 +1,3 @@
-#ifndef __MODULO_H__
-#define __MODULO_H__
-/*********************************************
-* Fecha: 18 de Febrero 2025
-* Autor: Tomás Ospina Ulloa.
-* Materia: Sistemas Operativos
-* Tema: Memoria dinamica y operacion de multiplicacion de matrices, complejidad, creacion de benchmark, programacion modular y autom>**********************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include "modulo.h"
@@ -43,4 +36,61 @@ void multiplicar_Matriz(int *Ma,int *Mb,int *Mc,int s){
                                 Mc[i*s+j] = Suma;
                         }
                 }
+}
+
+void vectorInicio(vectorDinamico *V){
+        V->capacidad = CAPACIDAD_INICIAL;
+        V->totalElementos = 0;
+        V->elementos = malloc(sizeof(void *) * V->capacidad); 
+}
+
+int totalVector(vectorDinamico *V){
+        return V->totalElementos;
+}
+
+static void resizeVector(vectorDinamico *V, int capacidad){
+        printf("Redimensión: %d a %d \n", V->capacidad, capacidad);
+
+        void **elementos = realloc(V->elementos, sizeof(void *) * capacidad);
+        if(elementos){
+                V->elementos = elementos;
+                V->capacidad = capacidad;
+        }
+}
+
+void addVector(vectorDinamico *V, void *elemento){
+        if(V->capacidad == V->totalElementos)
+                resizeVector(V, V->capacidad*2);
+        V->elementos[V->totalElementos++] = elemento;
+}
+
+void freeVector(vectorDinamico *V){
+        free(V->elementos);
+}
+
+void *getVector(vectorDinamico *V, int indice){
+        if(indice >= 0 && indice < V->totalElementos)
+                return V->elementos[indice];
+        return NULL;
+}
+
+void setVector(vectorDinamico *V, int indice, void *elemento){
+        if(indice >= 0 && indice < V->totalElementos)
+                V->elementos[indice]=elemento;
+}
+
+
+void borrarVector(vectorDinamico *V, int indice){
+        if(indice < 0 || indice >= V->totalElementos)
+                return; 
+
+        V->elementos[indice] = NULL;
+
+        for(int i=indice; i<V->totalElementos-1; i++){
+                V->elementos[i] = V->elementos[i+1];
+                V->elementos[i+1] = NULL; 
+        }
+        V->totalElementos--;
+        if(V->totalElementos>0 && V->totalElementos == V->capacidad/4)
+                resizeVector(V, V->capacidad/2);
 }
