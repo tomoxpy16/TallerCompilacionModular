77c638c6d631ed07a76b454113774fcc0530ba91
tomoxpy16 <tomasospina16@gmail.com>
2025-02-24 21:29:41 -0500

    Update Modulo.h

--- Modulo.h
+++ Modulo.h
@@ -10,5 +10,13 @@
 void impMatriz(int *matriz, int S);
 void inicializar_matriz(int *Ma,int *Mb, int s);
 void multiplicar_Matriz(int *Ma,int *Mb,int *Mc,int s);
+void vectorInicio(vectorDinamico *V);
+int totalVector(vectorDinamico *V);
+static void resizeVector(vectorDinamico *V, int capacidad);
+void addVector(vectorDinamico *V, void *elemento);
+void freeVector(vectorDinamico *V);
+void *getVector(vectorDinamico *V, int indice);
+void setVector(vectorDinamico *V, int indice, void *elemento);
+void borrarVector(vectorDinamico *V, int indice);
 
 #endif
