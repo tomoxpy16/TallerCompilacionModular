2add2b7afa1bf78b5b486c44bdfffe81cbe92bb1
tomoxpy16 <tomasospina16@gmail.com>
2025-02-24 21:05:26 -0500

    Create Modulo.c

---
+++ Modulo.c
@@ -0,0 +1,46 @@
+#ifndef __MODULO_H__
+#define __MODULO_H__
+/*********************************************
+* Fecha: 18 de Febrero 2025
+* Autor: Tomás Ospina Ulloa.
+* Materia: Sistemas Operativos
+* Tema: Memoria dinamica y operacion de multiplicacion de matrices, complejidad, creacion de benchmark, programacion modular y autom>**********************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include "modulo.h"
+
+void impMatriz(int *matriz, int S)
+{
+        for(int i=0; i<S;i++)
+        {       for(int j=0; j<S;j++)
+                        {
+                        printf(" %d ", matriz[i*S+j]);
+                        }
+        printf("\n");
+        }
+        printf("----------------------------------------\n");
+}
+
+void inicializar_matriz(int *Ma,int *Mb, int s)
+{
+        for(int i=0; i<s*s;i++)
+        {
+        Ma[i] = 2*i +1;
+        Mb[i] = 3*i;
+        }
+}
+void multiplicar_Matriz(int *Ma,int *Mb,int *Mc,int s){
+        for(int i= 0; i<s;i++){
+                for(int j = 0;j<s;j++){
+
+                        int Suma = 0;
+                        int *pA, *pB;
+                        pA = Ma + i*s;
+                        pB = Mb + j;
+                        for(int k = 0 ; k<s;k++,pA++,pB+=s){
+                        Suma += (*pA * *pB);
+                        }
+                                Mc[i*s+j] = Suma;
+                        }
+                }
+}
